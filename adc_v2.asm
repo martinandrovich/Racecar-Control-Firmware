cb9654c53279f0cdf8e0abe0fb2902de1ab13f4b
DanielRobTek17 <36709407+DanielRobTek17@users.noreply.github.com>
2018-04-17 15:52:01 +0200

    adc update

--- adc_v2.asm
+++ adc_v2.asm
@@ -7,20 +7,27 @@
 
 
 RCALL SET_POINTERAVG_X
+
 RCALL SETUP_SRAM
-RCALL SET_POINTERAVG_X
+
+;;make recent X_AVG_H
 
 ;____________ADC IS FIRST________________
 ;------------------------------------------------------------------------------;
-RCALL ADC
+
+ADC_MOVING:
 
 LDS XH, RECENT_XAVG_H							;
 LDS XL, RECENT_XAVG_L							;
+
 ST	X+, ADC_H									;insert ADC_val from high because of ADLAR
 
 CPI XL, AVGSIZE									;check for reset
+
 BRNE SKIP_THIS_RESET							;
+
 RCALL SET_POINTERAVG_X							;	
+
 SKIP_THIS_RESET:								;
 
 STS RECENT_XAVG_L, XL							;
@@ -29,19 +36,26 @@ RCALL ADD_LOOP									;add loop
 
 RCALL DIV_LOOP									;
 
+
 RET
 
 ;------------------------------------------------------------------------------;
 ;____________POINTER_FOR_MEANFILTER_______
 ;------------------------------------------------------------------------------;
+
 SET_POINTERAVG_X:
 
-LDI XH, HIGH(MOVAVG);
-LDI XL, LOW(MOVAVG);
+
+LDI XH, HIGH(MOVAVG)							;
+LDI XL, LOW(MOVAVG)								;
+
 
 RET
+
 ;____________ADD_LOOP____________
 ;------------------------------------------------------------------------------;
+
+
 ADD_LOOP:
 CLR		TEMP1										;
 CLR		TEMP2										;
@@ -52,12 +66,13 @@ ADDER_LOOP:											;
 LD		TEMP1, X+ 									;load from pointer
 ADD		TEMP2, TEMP1								;
 
-BRCC	SKIP_INCREMENT_H							;branch if carry is not set
+BRCC	SKIP_INCREMENT_H							;branch if carry is not set %% can change to SBIC, SREG <- 
 INC		TEMP3										;
 SKIP_INCREMENT_H:									;
 
-CPI		XL, LOW(MOVAVG_END)							;
+CPI		XL, LOW(MOVAVG_END)							;remember to compare ZH aswell.
 BRNE	ADDER_LOOP									;
+
 RET													;
 
 ;____________DIV_LOOP____________
@@ -67,14 +82,14 @@ DIV_LOOP:
 LDI		TEMP1, AVGDIV								;
 
 DIVIDE_LOOP_CONTINUE:								;
-CLC													;might be removed, not sure
+
 ASR		TEMP3										;TEMP3 IS HIGH
 ROR		TEMP2										;TEMP2 IS LOW
 
 DEC		TEMP1										;
 BRNE	DIVIDE_LOOP_CONTINUE						;
 
-STS		ACC_L, TEMP2								;save val
+STS		ACCELEROMETER, TEMP2						;save val
 
 RET													;
 
