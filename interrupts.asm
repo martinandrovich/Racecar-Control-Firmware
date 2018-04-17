c6601ff6153addfe5974e99966cd4d6a51080e6e
DanielRobTek17 <36709407+DanielRobTek17@users.noreply.github.com>
2018-04-17 09:37:31 +0200

    interrupts added
    move to main

---
+++ interrupts.asm
@@ -0,0 +1,68 @@
+; ######____________________________________________________________________
+; >> INTERRUPT SERVICE ROUTINES
+
+INT0_ISR:
+	
+	LDS		TEMPI, FUNC_FLG														; Load Function Flags from SRAM into Temporary Interrupt Register
+	SET																			; Set T flag
+	BLD		TEMPI, TACHO														; Set BIT in Temporary Interrupt Register
+	STS		FUNC_FLG, TEMPI														; Store Function Flags to SRAM
+
+	RETI																		; Return
+
+INT1_ISR:
+	
+	LDS		TEMPI, FUNC_FLG														; Load Function Flags from SRAM into Temporary Interrupt Register
+	SET																			; Set T flag
+	BLD		TEMPI, FNLNE														; Set BIT in Temporary Interrupt Register
+	STS		FUNC_FLG, TEMPI														; Store Function Flags to SRAM
+
+	RETI																		; Return
+
+INT2_ISR:
+	
+	LDS		TEMPI, FUNC_FLG														; Load Function Flags from SRAM into Temporary Interrupt Register
+	SET																			; Set T flag
+	BLD		TEMPI, MISSING														; Set BIT in Temporary Interrupt Register
+	STS		FUNC_FLG, TEMPI														; Store Function Flags to SRAM
+
+	RETI																		; Return
+
+TMR1_ISR:
+	
+	LDS		TEMPI, FUNC_FLG														; Load Function Flags from SRAM into Temporary Interrupt Register
+	SET																			; Set T flag
+	BLD		TEMPI, TMR1															; Set BIT in Temporary Interrupt Register
+	STS		FUNC_FLG, TEMPI														; Store Function Flags to SRAM
+
+	RETI																		; Return
+
+TELEGRAM_ISR:
+	
+	; !!! Race Condition! Flag should be cleared instead.
+	
+	IN		RXREG, UDR															; Read UDR register
+	
+	LDS		TEMPI, FUNC_FLG														; Load Function Flags from SRAM into Temporary Interrupt Register
+	SET																			; Set T flag
+	BLD		TEMPI, TELPD														; Set BIT in Temporary Interrupt Register
+	STS		FUNC_FLG, TEMPI														; Store Function Flags to SRAM
+
+	RETI																		; Return
+
+ADC_ISR:
+
+	LDS		TEMPI, FUNC_FLAG													; Load Function Flags from SRAM into Temporary Interrupt Register
+	SET																			; Set T flag
+	BLD		TEMPI, ACCLR														; Set BIT in Temporary Interrupt Register
+	STS		FUNC_FLG, TEMPI														; Store Function Flags to SRAM
+
+	IN		TEMPI, ADCL
+	NOP
+	STS		ADC_L, TEMPI
+
+	IN		TEMPI, ADCH
+	NOP
+	STS		ADC_H, TEMPI			
+
+	RETI
