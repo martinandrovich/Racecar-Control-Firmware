2dbe6436a0446a210f0b71971d6a58a3af3fd2d2
DanielRobTek17 <36709407+DanielRobTek17@users.noreply.github.com>
2018-04-17 11:43:10 +0200

    ADC

---
+++ adc_v2.asm
@@ -0,0 +1,90 @@
+//CONSTANTS ->
+.equ AVGSIZE = 128								;
+.equ AVGDIV	 = 7								; => 2^5 = 32
+
+.equ MOVAVG = 0x500								;
+.equ MOVAVG_END = MOVAVG+AVGSIZE				;
+
+
+RCALL SET_POINTERAVG_X
+RCALL SETUP_SRAM
+RCALL SET_POINTERAVG_X
+
+;____________ADC IS FIRST________________
+;------------------------------------------------------------------------------;
+RCALL ADC
+
+LDS XH, RECENT_XAVG_H							;
+LDS XL, RECENT_XAVG_L							;
+ST	X+, ADC_H									;insert ADC_val from high because of ADLAR
+
+CPI XL, AVGSIZE									;check for reset
+BRNE SKIP_THIS_RESET							;
+RCALL SET_POINTERAVG_X							;	
+SKIP_THIS_RESET:								;
+
+STS RECENT_XAVG_L, XL							;
+
+RCALL ADD_LOOP									;add loop
+
+RCALL DIV_LOOP									;
+
+RET
+
+;------------------------------------------------------------------------------;
+;____________POINTER_FOR_MEANFILTER_______
+;------------------------------------------------------------------------------;
+SET_POINTERAVG_X:
+
+LDI XH, HIGH(MOVAVG);
+LDI XL, LOW(MOVAVG);
+
+RET
+;____________ADD_LOOP____________
+;------------------------------------------------------------------------------;
+ADD_LOOP:
+CLR		TEMP1										;
+CLR		TEMP2										;
+CLR		TEMP3										;
+RCALL	SET_POINTERAVG_X
+
+ADDER_LOOP:											;
+LD		TEMP1, X+ 									;load from pointer
+ADD		TEMP2, TEMP1								;
+
+BRCC	SKIP_INCREMENT_H							;branch if carry is not set
+INC		TEMP3										;
+SKIP_INCREMENT_H:									;
+
+CPI		XL, LOW(MOVAVG_END)							;
+BRNE	ADDER_LOOP									;
+RET													;
+
+;____________DIV_LOOP____________
+;------------------------------------------------------------------------------;
+
+DIV_LOOP:
+LDI		TEMP1, AVGDIV								;
+
+DIVIDE_LOOP_CONTINUE:								;
+CLC													;might be removed, not sure
+ASR		TEMP3										;TEMP3 IS HIGH
+ROR		TEMP2										;TEMP2 IS LOW
+
+DEC		TEMP1										;
+BRNE	DIVIDE_LOOP_CONTINUE						;
+
+STS		ACC_L, TEMP2								;save val
+
+RET													;
+
+;____________SETUP_SRAM__________
+;------------------------------------------------------------------------------;
+SETUP_SRAM:											;
+LDI TEMP1, AVGSIZE									;
+
+CONTINUE_NULL:										;
+ST	X+, TEMP1 										;
+CPI XL, LOW(MOVAVG_END)								;
+BRNE CONTINUE_NULL									;
+RET													;
