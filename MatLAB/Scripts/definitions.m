6a44a776f46d65f9de011af0ef1fdafdab18f6e5
Martin Androvich <martinandrovich@gmail.com>
2018-04-26 13:20:00 +0200

    Restructured MatLab Code.

---
+++ MatLAB/Scripts/definitions.m
@@ -0,0 +1,31 @@
+% Clear everything
+clear;
+clc;
+clf;
+close(gcf);
+
+% Definitions
+bmodule = Bluetooth('RNBT-E2A9', 1);
+
+broadcastModes  = struct(...
+                    'Disabled',         0,      ...                    
+                    'All',              8,      ...
+                    'Tachometer',       24,     ...
+                    'Finishline',       56,     ...
+                    'Accelerometer',    40      ...
+                  );           
+
+logDuration     = 10;
+logDistance     = 290;
+movingAvgSize   = 32;
+dutyCycle       = 90;
+stateEnabled    = false;
+
+data            = 0;
+dataLong        = uint16(0);
+dataAcclr       = uint8(0);
+dataTacho       = uint16(0);
+count           = 1;
+tachoVal        = 0;
+
+fprintf('Definitions OK.\n\n');
---
+++ MatLAB/Scripts/tacho.m
@@ -0,0 +1,71 @@
+% Global definitions
+run('definitions.m');
+
+% Tachometer Logging
+disp("Tachometer Logging [Ticks/s]");
+disp("Version 1.1.0");
+
+% Plot configuration
+plotTitle       = 'Tachometer Plot';
+xLabel          = 'Elapsed Time [s]';
+yLabel          = 'Ticks [-]';
+legend1         = 'Tachometer Value';
+yMax            =  inf;
+yMin            =  0;
+plotGrid        = 'on';
+
+% Connect to Bluetooth Module
+fopen(bmodule);
+fprintf('Connection established; starting data logging.');
+
+% Set broadcasting mode
+UnitController.setBroadcastMode(broadcastModes.Tachometer);
+
+% Start vehicle
+UnitController.setDutyCycle(dutyCycle);
+
+% Enable timer
+tic
+
+% Log data
+while tachoVal < logDistance
+    
+   dataBytes = fread(bmodule, 2);   
+   
+   dataLong = bitor(bitshift(dataBytes(1), 8), dataBytes(2));
+   tachoVal = dataLong;
+   %disp(tachoVal);
+   data(count) = dataLong;
+   
+   count = count + 1;
+  
+end
+
+% Stop vehicle & broadcasting
+UnitController.setDutyCycle(0);
+UnitController.setBroadcastMode(broadcastModes.Disabled);
+
+% Calculate elapsed time
+timeWaited = toc;
+timeActual = timeWaited/length(data);
+timeElapsed = 0 + timeActual : timeActual : timeWaited;
+
+% Plot data
+plotGraph = plot(timeElapsed, data, '-');
+hold on;
+title(plotTitle, 'FontSize', 15);
+xlabel(xLabel, 'FontSize', 15);
+ylabel(yLabel, 'FontSize', 15);
+ax = gca;
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+legend(legend1);
+axis([0 timeWaited yMin yMax]);
+grid(plotGrid);
+
+% Maximize figure window
+drawnow;
+set(get(handle(gcf), 'JavaFrame'), 'Maximized', 1);
+
+% Close connection
+fclose(bmodule);
